d7ac5c26e27c7899f11ed95e592a8540189c7597
Sakshi Madrecha <jain.sakshi1099@gmail.com>
2021-05-28 19:53:33 +0530

    added ui and implemented methods

---
+++ app/src/main/java/com/gkmit/todo/DatabaseHelper.java
@@ -0,0 +1,31 @@
+package com.gkmit.todo;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "ToDo app";
+    public static final String TABLE_NAME = "TODO_LIST";
+    public static final String COL1_TASK = "task";
+    public static final String _ID = BaseColumns._ID;
+
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqlDB) {
+        String createTodoListTable = "CREATE TABLE " + TABLE_NAME + " ( _id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL1_TASK + " TEXT)";
+        sqlDB.execSQL(createTodoListTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqlDB, int i, int i2) {
+        sqlDB.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqlDB);
+    }
+}
--- app/src/main/java/com/gkmit/todo/MainActivity.java
+++ app/src/main/java/com/gkmit/todo/MainActivity.java
@@ -1,14 +1,107 @@
 package com.gkmit.todo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private DatabaseHelper dbHelper;
+    private ArrayAdapter<String> mAdapter;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.list);
+        updateList();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add_action, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_add_task) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Add Todo Task Item");
+            builder.setMessage("Please write the task...");
+            final EditText todoET = new EditText(this);
+            builder.setView(todoET);
+            builder.setPositiveButton("Add Task", (dialogInterface, i) -> {
+                String todoTaskInput = todoET.getText().toString();
+                dbHelper = new DatabaseHelper(MainActivity.this);
+                SQLiteDatabase sqLiteDatabase = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.clear();
+                
+                values.put(dbHelper.COL1_TASK, todoTaskInput);
+                sqLiteDatabase.insertWithOnConflict(dbHelper.TABLE_NAME, null, values, SQLiteDatabase.CONFLICT_IGNORE);
+
+                updateList();
+                });
+
+                builder.setNegativeButton("Cancel", null);
+
+                builder.create().show();
+        }
+        return true;
+    }
+    private void updateList() {
+        ArrayList<String> taskList = new ArrayList<>();
+        dbHelper = new DatabaseHelper(MainActivity.this);
+        SQLiteDatabase sqLiteDatabase = dbHelper.getReadableDatabase();
+        
+        Cursor cursor = sqLiteDatabase.query(dbHelper.TABLE_NAME,
+                new String[]{dbHelper._ID, dbHelper.COL1_TASK},
+                null, null, null, null, null);
+        
+        while (cursor.moveToNext()) {
+            int index = cursor.getColumnIndex(dbHelper.COL1_TASK);
+            taskList.add(cursor.getString(index));
+        }
+        
+        if (mAdapter == null) {
+            mAdapter = new ArrayAdapter<String>(this, R.layout.todotask, R.id.textview_task_title, taskList);
+            listView.setAdapter(mAdapter);
+        } else {
+            mAdapter.clear();
+            mAdapter.addAll(taskList);
+            mAdapter.notifyDataSetChanged();
+        }
+        cursor.close();
+        dbHelper.close();
+    }
+
+    public void onDoneButtonClick(View view) {
+        View v = (View) view.getParent();
+        TextView textViewToDo =  v.findViewById(R.id.textview_task_title);
+        String taskItem = textViewToDo.getText().toString();
+
+        String deleteTodoItemSql = "DELETE FROM " + dbHelper.TABLE_NAME +
+                " WHERE " + dbHelper.COL1_TASK + " = '" + taskItem + "'";
+
+        dbHelper = new DatabaseHelper(MainActivity.this);
+        SQLiteDatabase sqlDB = dbHelper.getWritableDatabase();
+        sqlDB.execSQL(deleteTodoItemSql);
+        updateList();
+    }
+
 }
